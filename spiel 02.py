92a359b97bdcba0d0ad4fcb689549af65427b437
Programmer06 <45739297+Programmer06@users.noreply.github.com>
2020-07-03 10:27:00 +0200

    Update spiel 02.py

--- spiel 02.py
+++ spiel 02.py
@@ -11,7 +11,7 @@ schwarz = [2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35]
 
 
 def spiel():
-    print("Auf was willst du setzen? (Bitte gib entweder Farbe, Reihe oder Nummer ein) ")
+    print("Auf was willst du setzen? (Bitte gib entweder Farbe, Reihe oder Zahl ein) ")
     eingabe_methode = input()
 
     if eingabe_methode == "Reihe" :
